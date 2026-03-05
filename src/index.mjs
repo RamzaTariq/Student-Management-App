1e449003c85ae8ffba7fd654b29c7b8a2a8caade
Ramza <ramza_tariq@outlook.com>
2026-03-05 16:52:01 +0000

    Added mock student data

--- src/index.mjs
+++ src/index.mjs
@@ -1,24 +1,10 @@
-import express from "express";
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-
-//mock database
-let students = [
-    { id:1, username:"jburns", course:"information technology", module:"A001" },
-    { id:2, username:"rsmith", course:"political sciences", module:"C001" },
-    { id:3, username:"brown", course:"business administration", module:"D001" },
-    { id:4, username:"sjane", course:"information technology", module:"A001" }
+const mockStudents = [
+    {id:1, username:"jburns", course:"information technology", module:"A001"},
+    {id:2, username:"rsmith", course:"political sciences", module:"C001"},
+    {id:3, username:"jbrown", course:"business administration", module:"D001"},
+    {id:4, username:"sjane", course:"information technology", module:"A001"},
+    {id:5, username:"mtiller", course:"education", module:"F003"},
+    {id:6, username:"afoxy", course:"mechatronics", module:"E002"},
+    {id:7, username:"pmcdonald", course:"architecture", module:"B101"},
+    {id:8, username:"sfraser", course:"art", module:"A611"}
 ];
-
-// Root route
-app.get("/", (req,res)=>{
-    res.send("Student API running");
-});
-
-app.listen(PORT, ()=>{
-    console.log(`Server running on https://localhost:${PORT}`);
-});
-
