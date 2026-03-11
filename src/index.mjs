ecb9eab1cec915778c65c74e8515b70c1e1b1c04
Ramza <ramza_tariq@outlook.com>
2026-03-11 12:55:54 +0000

    minor changes

--- src/index.mjs
+++ src/index.mjs
@@ -3,22 +3,13 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+// middleware to read JSON
 app.use(express.json());
 
-//mock database
-let students = [
-    { id:1, username:"jburns", course:"information technology", module:"A001" },
-    { id:2, username:"rsmith", course:"political sciences", module:"C001" },
-    { id:3, username:"brown", course:"business administration", module:"D001" },
-    { id:4, username:"sjane", course:"information technology", module:"A001" }
-];
-
-// Root route
-app.get("/", (req,res)=>{
-    res.send("Student API running");
+app.get("/", (req, res) => {
+  res.send("Student API Running");
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on https://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
